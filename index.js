b3420d12fa79a1e3521c0412cf51d5cbfab29d28
Anurag <anuragchakravorty000@gmail.com>
2022-10-10 11:11:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// add a counter 
+
+var button = document.getElementById("clickme")
+var count = 0;
+
+button.onclick = function() {
+    count = count+1 ;
+    // button.innerHTML = 'Click me: ' + count; 
+    function display() {
+        button.innerHTML = 'Click me: ' + count;
+    }
+    display()
+}
+
+
+var button2 = document.getElementById("zero")
+button2.onclick = function() {
+    count = 0;
+    button.innerHTML = 'Click me: ' + count;
+}
