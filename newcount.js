4467e5951519b15b249c3118d8fe4707c82f7c1d
Anurag <anuragchakravorty000@gmail.com>
2022-11-17 13:43:49 +0530

    setTime js

--- newcount.js
+++ newcount.js
@@ -3,9 +3,13 @@ function outer() {
     function incrementCounter() {
         counter = counter +1;
         return counter
+        // When we do not return the counter, the output is always undefned.
     }
     return incrementCounter;
 }
 const newFunction = outer();
 console.log(newFunction())
 console.log(newFunction())
+const anotherFunction = outer();
+console.log(anotherFunction())
+console.log(anotherFunction())
